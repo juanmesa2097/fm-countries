b538f1ded9ae890179fef149d2c6675af66f77fa
juanmesa2097 <juanmesa2097@gmail.com>
2020-05-30 04:12:56 -0500

    Add headings styles and fix general things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutModule } from './ui/layout/layout.module';
 import { PageResponsesModule } from './pages/page-responses/page-responses.module';
+import { LayoutModule } from './ui/layout/layout.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LayoutModule,
-    PageResponsesModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, PageResponsesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
