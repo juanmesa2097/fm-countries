259d39380859370386ba5aa0e41b22ecc65f06ee
juanmesa2097 <juanmesa2097@gmail.com>
2020-05-30 23:04:44 -0500

    Implement countries cards layout

--- src/app/pages/features/countries/countries.module.ts
+++ src/app/pages/features/countries/countries.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CountriesRoutingModule } from './countries-routing.module';
 import { CountriesPage } from './pages/countries/countries.page';
+import { CountryCardComponent } from './components/country-card/country-card.component';
 
 @NgModule({
-  declarations: [CountriesPage],
+  declarations: [CountriesPage, CountryCardComponent],
   imports: [CommonModule, CountriesRoutingModule],
 })
 export class CountriesModule {}
